0520b8b206d84c4189872c614e6c59b95bd5d76f
Marcin Grzejszczak <marcin@grzejszczak.pl>
2017-02-19 23:01:28 +0100

    Added discovery example

---
+++ consumer_with_discovery/src/main/java/com/example/BeerController.java
@@ -0,0 +1,90 @@
+package com.example;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+@RestController
+class BeerController {
+
+	private final RestTemplate restTemplate;
+
+	BeerController(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	@RequestMapping(method = RequestMethod.POST,
+			value = "/beer",
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String gimmeABeer(@RequestBody Person person) throws MalformedURLException {
+		ResponseEntity<Response> response = this.restTemplate.exchange(
+				RequestEntity
+						.post(URI.create("http://somenameforconsumer/check"))
+						.contentType(MediaType.APPLICATION_JSON)
+						.body(person),
+				Response.class);
+		switch (response.getBody().status) {
+		case OK:
+			return "THERE YOU GO";
+		default:
+			return "GET LOST";
+		}
+	}
+}
+
+class Person {
+	public String name;
+	public int age;
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public Person() {
+	}
+}
+
+class Response {
+	public ResponseStatus status;
+}
+
+enum ResponseStatus {
+	OK, NOT_OK
+}
+
+
+
+/*
+
+	@RequestMapping(method = RequestMethod.POST,
+			value = "/beer",
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String gimmeABeer(@RequestBody Person person) throws MalformedURLException {
+		ResponseEntity<Response> response = this.restTemplate.exchange(
+				RequestEntity
+						.post(URI.create("http://localhost:8090/check"))
+						.contentType(MediaType.APPLICATION_JSON)
+						.body(person),
+				Response.class);
+		switch (response.getBody().status) {
+		case OK:
+			return "THERE YOU GO";
+		default:
+			return "GET LOST";
+		}
+	}
+
+
+ */
---
+++ consumer_with_discovery/src/test/java/com/example/BeerControllerTest.java
@@ -0,0 +1,72 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@AutoConfigureStubRunner(workOffline = true, ids = "com.example:beer-api-producer:+:stubs:8090")
+@DirtiesContext
+public class BeerControllerTest extends AbstractTest {
+
+	@Autowired MockMvc mockMvc;
+
+	@Test public void should_give_me_a_beer_when_im_old_enough() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 22)).getJson()))
+				.andExpect(status().isOk())
+				.andExpect(content().string("THERE YOU GO"));
+	}
+
+	@Test public void should_reject_a_beer_when_im_too_young() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 17)).getJson()))
+				.andExpect(status().isOk())
+				.andExpect(content().string("GET LOST"));
+	}
+}
+
+
+/*
+
+
+
+	@Test public void should_give_me_a_beer_when_im_old_enough() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 22)).getJson()))
+				.andExpect(status().isOk())
+				.andExpect(content().string("THERE YOU GO"));
+	}
+
+	@Test public void should_reject_a_beer_when_im_too_young() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 17)).getJson()))
+				.andExpect(status().isOk())
+				.andExpect(content().string("GET LOST"));
+	}
+
+ */
+
