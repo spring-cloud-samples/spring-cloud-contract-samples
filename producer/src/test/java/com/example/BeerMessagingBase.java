79dc164608e127429ed5cac19312538af4f32c20
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-05-10 11:57:16 +0200

    Removing implementations

--- producer/src/test/java/com/example/BeerMessagingBase.java
+++ producer/src/test/java/com/example/BeerMessagingBase.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.util.concurrent.TimeUnit;
+import javax.inject.Inject;
+
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +11,11 @@ import org.springframework.cloud.contract.verifier.messaging.MessageVerifier;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.inject.Inject;
-import java.util.concurrent.TimeUnit;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ProducerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @AutoConfigureMessageVerifier
 public abstract class BeerMessagingBase {
-
+	//remove::start[]
 	@Inject MessageVerifier messaging;
 	@Autowired PersonCheckingService personCheckingService;
 
@@ -33,5 +33,5 @@ public abstract class BeerMessagingBase {
 	public void clientIsTooYoung() {
 		personCheckingService.shouldGetBeer(new PersonToCheck(5));
 	}
-	
+	//remove::end[]
 }
