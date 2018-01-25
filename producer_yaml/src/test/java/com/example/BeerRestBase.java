7578f9ba713a56f4ea5a694f98a40e9c54709427
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-01-25 21:32:17 +0100

    Fixed broken compilation

--- producer_yaml/src/test/java/com/example/BeerRestBase.java
+++ producer_yaml/src/test/java/com/example/BeerRestBase.java
@@ -1,16 +1,14 @@
 package com.example;
 
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
-
 import java.util.Random;
 
-import org.hamcrest.Description;
-import org.hamcrest.TypeSafeMatcher;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.anyString;
@@ -31,16 +29,8 @@ public abstract class BeerRestBase {
 		RestAssuredMockMvc.standaloneSetup(producerController, statsController);
 	}
 
-	private TypeSafeMatcher<PersonToCheck> oldEnough() {
-		return new TypeSafeMatcher<PersonToCheck>() {
-			@Override protected boolean matchesSafely(PersonToCheck personToCheck) {
-				return personToCheck.age >= 20;
-			}
-
-			@Override public void describeTo(Description description) {
-
-			}
-		};
+	private ArgumentMatcher<PersonToCheck> oldEnough() {
+		return argument -> argument.age >= 20;
 	}
 	//remove::end[]
 }
