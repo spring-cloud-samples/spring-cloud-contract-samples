b4f0d332b1ece07b90b58ee54eaddfd0d33fc8e1
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-01-31 17:57:42 +0100

    Fixed not compiling code

--- consumer/src/test/java/com/example/intoxication/IntoxicationControllerYamlTest.java
+++ consumer/src/test/java/com/example/intoxication/IntoxicationControllerYamlTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -31,7 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
 //remove::start[]
-@AutoConfigureStubRunner(workOffline = true, ids = "com.example:beer-api-producer-yaml")
+@AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.LOCAL, ids = "com.example:beer-api-producer-yaml")
 //remove::end[]
 @DirtiesContext
 //@org.junit.Ignore
@@ -70,5 +71,3 @@ public class IntoxicationControllerYamlTest extends AbstractTest {
 		//remove::end[]
 	}
 }
-
-
