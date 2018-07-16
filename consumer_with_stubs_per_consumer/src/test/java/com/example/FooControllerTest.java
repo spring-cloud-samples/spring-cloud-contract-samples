42fa739c51f64539f646721fa82a4280184b31fd
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-07-16 16:02:44 +0200

    Fixed compilation issues

--- consumer_with_stubs_per_consumer/src/test/java/com/example/FooControllerTest.java
+++ consumer_with_stubs_per_consumer/src/test/java/com/example/FooControllerTest.java
@@ -1,29 +1,16 @@
 package com.example;
 
 import org.assertj.core.api.BDDAssertions;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.client.RestTemplate;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Client used (with small adaptation) in a SC contract blog post
@@ -41,7 +28,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //@org.junit.Ignore
 public class FooControllerTest {
 
-	@StubRunnerPort("beer-api-producer-with-stubs-per-consumer") int producerPort;
+	//remove::start[]
+	@StubRunnerPort("beer-api-producer-with-stubs-per-consumer")
+	//remove::end[]
+	int producerPort;
 
 	@Test
 	public void should_return_foo_for_foo_consumer() {
