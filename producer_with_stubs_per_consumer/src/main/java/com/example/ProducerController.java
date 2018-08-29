067b974964c9480930b85a55e2e65815992e923f
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-08-29 16:11:31 +0200

    Clone

--- producer_with_stubs_per_consumer/src/main/java/com/example/ProducerController.java
+++ producer_with_stubs_per_consumer/src/main/java/com/example/ProducerController.java
@@ -19,14 +19,7 @@ public class ProducerController {
 			consumes="application/json",
 			produces="application/json")
 	public Response check(@RequestBody PersonToCheck personToCheck) {
-		//remove::start[]
-		//tag:impl[]
-		if (personCheckingService.shouldGetBeer(personToCheck)) {
-			return new Response(BeerCheckStatus.OK, personToCheck.name);
-		}
-		return new Response(BeerCheckStatus.NOT_OK, personToCheck.name);
-		//end:impl[]
-		//remove::end[return]
+		return null;
 	}
 	
 }
@@ -49,14 +42,9 @@ class PersonToCheck {
 
 class Response {
 	public BeerCheckStatus status;
-	public String name;
-	public String surname;
 
-	// we're setting the name to both fields
-	Response(BeerCheckStatus status, String name) {
+	Response(BeerCheckStatus status) {
 		this.status = status;
-		this.name = name;
-		this.surname = name;
 	}
 }
 
