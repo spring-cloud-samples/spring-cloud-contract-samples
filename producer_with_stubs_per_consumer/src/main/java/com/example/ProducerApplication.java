26d7ce390cb8172670a7874772a6dfde655332d7
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-09-05 17:13:22 +0200

    Removed code

--- producer_with_stubs_per_consumer/src/main/java/com/example/ProducerApplication.java
+++ producer_with_stubs_per_consumer/src/main/java/com/example/ProducerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
-@EnableBinding(Source.class)
 public class ProducerApplication {
 
 	public static void main(String[] args) {
