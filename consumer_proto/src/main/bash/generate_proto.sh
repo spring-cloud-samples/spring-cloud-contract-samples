0b93e7d5020b20742115f0b5018a7f695191aa40
Marcin Grzejszczak <marcin@grzejszczak.pl>
2019-02-19 13:21:49 +0100

    Removed artificial lib env passing

--- consumer_proto/src/main/bash/generate_proto.sh
+++ consumer_proto/src/main/bash/generate_proto.sh
@@ -6,10 +6,6 @@ set -o pipefail
 
 ./src/main/bash/download_protoc.sh
 
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}"
-[[ -z "${LD_LIBRARY_PATH}" ]] && LD_LIBRARY_PATH="$( whereis libatomic.so.1 | awk '{ print $2 }' )"
-echo "${LD_LIBRARY_PATH}"
-
 echo "Generating proto files"
 mkdir -p target/generated-sources/java
 target/protoc/bin/protoc --java_out=target/generated-sources/java src/main/resources/proto/beer.proto
