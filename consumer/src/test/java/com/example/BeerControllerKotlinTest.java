26dd8e8676ddb44c551ebb2dfcee956265576816
Marcin Grzejszczak <marcin@grzejszczak.pl>
2019-02-20 13:30:19 +0100

    Polish

--- consumer/src/test/java/com/example/BeerControllerKotlinTest.java
+++ consumer/src/test/java/com/example/BeerControllerKotlinTest.java
@@ -11,8 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+//remove::start[]
 import org.springframework.cloud.contract.stubrunner.junit.StubRunnerRule;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+//remove::end[]
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
