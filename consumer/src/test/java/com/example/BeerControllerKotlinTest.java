8008d14705715f02995c175e957d4087b32db064
Marcin Grzejszczak <marcin@grzejszczak.pl>
2019-02-20 13:28:25 +0100

    Made the Kotlin test not executed for compatibility build

--- consumer/src/test/java/com/example/BeerControllerKotlinTest.java
+++ consumer/src/test/java/com/example/BeerControllerKotlinTest.java
@@ -1,22 +1,24 @@
 package com.example;
 
+import org.junit.Assume;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort;
+import org.springframework.cloud.contract.stubrunner.junit.StubRunnerRule;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.StringUtils;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,12 +29,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-@AutoConfigureJsonTesters
-//remove::start[]
-// example of usage with fixed port
-//@AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.LOCAL, ids = "com.example:beer-api-producer:+:stubs:8090")
-@AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.LOCAL, ids = "com.example:beer-api-producer-kotlin")
-//remove::end[]
 @DirtiesContext
 public class BeerControllerKotlinTest extends AbstractTest {
 
@@ -42,15 +38,34 @@ public class BeerControllerKotlinTest extends AbstractTest {
 	BeerController beerController;
 
 	//remove::start[]
-	@StubRunnerPort("beer-api-producer-kotlin")
-	int producerPort;
+	@Rule
+	public StubRunnerRule rule = new StubRunnerRule()
+			.downloadStub("com.example","beer-api-producer-kotlin")
+			.stubsMode(StubRunnerProperties.StubsMode.LOCAL);
+	//remove::end[]
+
+	@BeforeClass
+	public static void beforeClass() {
+		Assume.assumeTrue("Spring Cloud Contract must be in version at least 2.1.0", atLeast210());
+		Assume.assumeTrue("Env var OLD_PRODUCER_TRAIN must not be set", StringUtils.isEmpty(System.getenv("OLD_PRODUCER_TRAIN")));
+	}
 
 	@Before
 	public void setupPort() {
-		this.beerController.port = this.producerPort;
+		// remove::start[]
+		this.beerController.port = this.rule.findStubUrl("beer-api-producer-kotlin").getPort();
+		// remove::end[]
 	}
 
-	//remove::end[]
+	private static boolean atLeast210() {
+		try {
+			Class.forName("org.springframework.cloud.contract.verifier.util.ContractVerifierUtil");
+		} catch (Exception ex) {
+			return false;
+		}
+		return true;
+	}
+
 	//tag::tests[]
 	@Test
 	public void should_give_me_a_beer_when_im_old_enough() throws Exception {
