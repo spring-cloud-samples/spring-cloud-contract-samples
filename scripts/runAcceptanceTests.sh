3271d9ee149f0895e4bbaeae1276e7a6ee362625
Marcin Grzejszczak <marcin@grzejszczak.pl>
2019-04-16 08:23:25 +0400

    Hoxton

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -32,7 +32,7 @@ export SKIP_COMPATIBILITY="${SKIP_COMPATIBILITY:-false}"
 if [[ "${SKIP_COMPATIBILITY}" != "true" ]]; then
 	startDockerCompose
   # TODO: Go back to snapshots one day
-  export CURRENT_BOOT_VERSION="2.1.3.RELEASE"
+  export CURRENT_BOOT_VERSION="2.2.0.BUILD-SNAPSHOT"
 	. ${ROOT}/scripts/runCompatibilityBuild.sh
 fi
 
