11e6327ed5b74d203438608f9cb28e4a7206036f
Marcin Grzejszczak <marcin@grzejszczak.pl>
2019-09-03 09:41:11 +0200

    Should work

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,13 +5,14 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
-ROOT=${ROOT:-`pwd`}
+export ROOT="${ROOT:-`pwd`}"
 
 function setup_git() {
     local git_name
     git_name="${1}"
     echo -e "\n\nCopying git repo to ${ROOT}/target/${git_name}\n\n"
     mkdir -p "${ROOT}/target/${git_name}"
+    rm -rf "${ROOT}/target/${git_name}"
     cp -R "${ROOT}/${git_name}" "${ROOT}/target/"
     mv "${ROOT}/target/${git_name}/git" "${ROOT}/target/${git_name}/.git"
     ls -al "${ROOT}/target/${git_name}"
