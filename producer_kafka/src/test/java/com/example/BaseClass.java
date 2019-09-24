0ae751d8704aa5a832f461b9ea72fa2f92bfed06
Marcin Grzejszczak <marcin@grzejszczak.pl>
2019-09-24 15:31:22 +0200

    Fixed missing removal tags

--- producer_kafka/src/test/java/com/example/BaseClass.java
+++ producer_kafka/src/test/java/com/example/BaseClass.java
@@ -20,15 +20,19 @@ import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+// remove::start[]
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+// remove::end[]
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+// remove::start[]
 @AutoConfigureMessageVerifier
 @EmbeddedKafka(partitions = 1, topics = {"topic1"})
+// remove::end[]
 @ActiveProfiles("test")
 public abstract class BaseClass {
 
