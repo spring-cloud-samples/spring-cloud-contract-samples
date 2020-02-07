77ba498b4dcffa6886d75fe3e449216a62de2f5a
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-02-07 10:38:48 +0100

    Router function is conditional for compatibility build

--- consumer/src/test/java/com/example/BeerControllerRouterFunctionTest.java
+++ consumer/src/test/java/com/example/BeerControllerRouterFunctionTest.java
@@ -1,6 +1,9 @@
 package com.example;
 
+import org.junit.Assume;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.cloud.contract.stubrunner.junit.StubRunnerRule;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
@@ -15,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.StringUtils;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,16 +31,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
-//remove::start[]
-@AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.LOCAL, ids = "com.example:beer-api-producer-routerfunction-webtestclient")
-//remove::end[]
 public class BeerControllerRouterFunctionTest extends AbstractTest {
 
 	@Autowired MockMvc mockMvc;
 	@Autowired BeerController beerController;
 
 	//remove::start[]
-	@StubRunnerPort("beer-api-producer-routerfunction-webtestclient") int producerPort;
+	@StubRunnerPort("com.example:beer-api-producer-routerfunction-webtestclient") int producerPort;
+
+	//remove::start[]
+	@Rule
+	public StubRunnerRule rule = new StubRunnerRule()
+			.downloadStub("com.example","beer-api-producer-routerfunction-webtestclient", "0.0.1.BUILD-SNAPSHOT")
+			.stubsMode(StubRunnerProperties.StubsMode.LOCAL);
+	//remove::end[]
+
+	@BeforeClass
+	public static void beforeClass() {
+		Assume.assumeTrue("Spring Cloud Contract must be in version at least 2.2.0", atLeast220());
+		Assume.assumeTrue("Env var OLD_PRODUCER_TRAIN must not be set", StringUtils.isEmpty(System.getenv("OLD_PRODUCER_TRAIN")));
+	}
+	
+	private static boolean atLeast220() {
+		try {
+			Class.forName("org.springframework.cloud.contract.spec.internal.DslPropertyConverter");
+		} catch (Exception ex) {
+			return false;
+		}
+		return true;
+	}
 
 	@Before
 	public void setupPort() {
