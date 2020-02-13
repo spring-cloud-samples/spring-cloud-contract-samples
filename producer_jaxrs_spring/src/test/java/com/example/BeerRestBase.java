384ee06bd99a612e9f8b1b8178e3da84b94ff91d
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-02-13 13:50:42 +0100

    Moar JUNIT5

--- producer_jaxrs_spring/src/test/java/com/example/BeerRestBase.java
+++ producer_jaxrs_spring/src/test/java/com/example/BeerRestBase.java
@@ -1,17 +1,15 @@
 package com.example;
 
 //remove::start[]
-
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 import com.example.beerapiproducerjaxrs.ProducerWithJaxRsApplication;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
+
 @SpringBootTest(classes = {ProducerWithJaxRsApplication.class},
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //remove::end[]
--- producer_jaxrs_spring/src/test/java/com/example/beerapiproducerjaxrs/ProducerWithJaxRsApplicationTests.java
+++ producer_jaxrs_spring/src/test/java/com/example/beerapiproducerjaxrs/ProducerWithJaxRsApplicationTests.java
@@ -1,9 +1,9 @@
 package com.example.beerapiproducerjaxrs;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 @SpringBootTest
 public class ProducerWithJaxRsApplicationTests {
 
