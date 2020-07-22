b159f395e95ac1fc24a9f437dacc031297971af0
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-07-22 16:29:50 +0200

    Should help with backward compatibility

--- consumer/src/test/java/com/example/AbstractTest.java
+++ consumer/src/test/java/com/example/AbstractTest.java
@@ -3,11 +3,16 @@ package com.example;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.BeforeEach;
+
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
 
 /**
  * @author Marcin Grzejszczak
  */
+// For backward compatibility
+@ImportAutoConfiguration(TestChannelBinderConfiguration.class)
 public abstract class AbstractTest {
 
 	public JacksonTester<Person> json;
