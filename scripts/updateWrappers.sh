cf71cb3f8114a2fd71ce2f41ff49ea276841faf5
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-09-01 12:10:45 +0200

    Bumped Gradle to 6.6.1

--- scripts/updateWrappers.sh
+++ scripts/updateWrappers.sh
@@ -6,7 +6,7 @@ set -o nounset
 set -o pipefail
 
 export ROOT="${ROOT:-`pwd`}"
-export WRAPPER_VERSION="${WRAPPER_VERSION:-6.4}"
+export WRAPPER_VERSION="${WRAPPER_VERSION:-6.6.1}"
 
 for d in consumer*
 do
