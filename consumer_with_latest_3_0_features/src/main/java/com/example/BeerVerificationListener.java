a6a95a1d97afa40c6243871b3fdce32bf6a52f0c
Shannon Pamperl <shanman190@gmail.com>
2020-11-08 17:38:22 -0600

    Add latest 3.0 Gradle features

---
+++ consumer_with_latest_3_0_features/src/main/java/com/example/BeerVerificationListener.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+@Component("input")
+class BeerVerificationListener implements Consumer<BeerVerificationListener.Verification> {
+
+	private static final Log log = LogFactory.getLog(BeerVerificationListener.class);
+
+	AtomicInteger eligibleCounter = new AtomicInteger();
+	AtomicInteger notEligibleCounter = new AtomicInteger();
+
+	@Override
+	public void accept(Verification verification) {
+		log.info("Received new verification");
+		//remove::start[]
+		//tag::listener[]
+		if (verification.eligible) {
+			this.eligibleCounter.incrementAndGet();
+		} else {
+			this.notEligibleCounter.incrementAndGet();
+		}
+		//end::listener[]
+		//remove::end[]
+	}
+
+	public static class Verification {
+		public boolean eligible;
+	}
+}
